b5b288f4504c16419f535b773121df15c338c6c4
paul_abib <paul_abib@connect-tech.sncf>
2022-08-30 14:04:24 +0200

    pourquoi j'ai encore ça en local ?

--- src/main/java/app/scrabble/rules/LetterValue.kt
+++ src/main/java/app/scrabble/rules/LetterValue.kt
@@ -7,7 +7,7 @@ class LetterValue : Rule {
 
     override fun calculateScore(word: Word): Score = Score(points = word.value.toList().sumOf { it.score() })
 
-    private fun Char.score(): Int = when (this.uppercaseChar()) {
+    private fun Char.score(): Int = when (uppercaseChar()) {
         'A', 'E', 'I', 'L', 'N', 'O', 'R', 'S', 'T', 'U' -> 1
         'D', 'G' -> 2
         'B', 'C', 'M', 'P' -> 3
